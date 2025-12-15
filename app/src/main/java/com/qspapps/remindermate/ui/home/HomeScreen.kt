882cb43f59fb8fb9d2993eabfff6e28bc3418aa8
Naveen Belkale <naveen@78health.com>
2025-12-15 17:01:09 +0530

    Added tooltip for hide completed and select date icons

--- app/src/main/java/com/qspapps/remindermate/ui/home/HomeScreen.kt
+++ app/src/main/java/com/qspapps/remindermate/ui/home/HomeScreen.kt
@@ -20,9 +20,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -54,6 +59,9 @@ fun HomeScreen(
     var showMenu by remember { mutableStateOf(false) }
     val context = LocalContext.current
     val currentTime by viewModel.currentTime.collectAsStateWithLifecycle()
+    val hideCompletedRemindersTooltipState = rememberTooltipState()
+    val showCompletedText = if (uiState.showCompleted) stringResource(id = R.string.hide_completed) else stringResource(id = R.string.show_completed)
+    val selectDateTooltipState = rememberTooltipState()
 
     val title = if (uiState.selectedDate.isEqual(LocalDate.now())) {
         stringResource(id = R.string.today_reminders)
@@ -73,15 +81,39 @@ fun HomeScreen(
             TopAppBar(
                 title = { Text(text = title) },
                 actions = {
-                    IconButton(onClick = { viewModel.toggleShowCompleted() }) {
-                        Icon(
-                            imageVector = if (uiState.showCompleted) Icons.Filled.Check else Icons.Filled.CheckCircle,
-                            contentDescription = if (uiState.showCompleted) stringResource(id = R.string.hide_completed) else stringResource(id = R.string.show_completed)
-                        )
-                    }
-                    IconButton(onClick = { showDatePicker = true }) {
-                        Icon(Icons.Default.DateRange, contentDescription = stringResource(id = R.string.select_date))
-                    }
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                            TooltipAnchorPosition.Above),
+                        tooltip = {
+                            PlainTooltip {
+                                Text(showCompletedText)
+                            }
+                        },
+                        state = hideCompletedRemindersTooltipState,
+                        content = {
+                            IconButton(onClick = { viewModel.toggleShowCompleted() }) {
+                                Icon(
+                                    imageVector = if (uiState.showCompleted) Icons.Filled.Check else Icons.Filled.CheckCircle,
+                                    contentDescription = showCompletedText
+                                )
+                            }
+                        }
+                    )
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                            TooltipAnchorPosition.Above),
+                        tooltip = {
+                            PlainTooltip {
+                                Text(stringResource(id = R.string.select_date))
+                            }
+                        },
+                        state = selectDateTooltipState,
+                        content = {
+                            IconButton(onClick = { showDatePicker = true }) {
+                                Icon(Icons.Default.DateRange, contentDescription = stringResource(id = R.string.select_date))
+                            }
+                        }
+                    )
                     Box {
                         IconButton(onClick = { showMenu = true }) {
                             Icon(Icons.Default.MoreVert, contentDescription = stringResource(id = R.string.more_options))
