fa09d6ade573a66315109bd82f5cbdc2045414a2
Naveen Belkale <naveen@78health.com>
2025-12-15 15:04:42 +0530

    Increased version to 2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.qspapps.remindermate"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.3"
+        versionCode = 5
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
