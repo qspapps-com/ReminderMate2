3faab1acfe1111c6ba1c24246d8664f6fadf885e
Naveen Belkale <naveen@78health.com>
2025-12-25 08:41:23 +0530

    Upgrading version to 2.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.qspapps.remindermate"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "2.0.7"
+        versionCode = 9
+        versionName = "2.0.8"
 
         testInstrumentationRunner = "com.qspapps.remindermate.HiltTestRunner"
     }
