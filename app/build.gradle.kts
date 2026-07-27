4d12369c8e211b46c29c4bb9624102b182c3d01f
Naveen Belkale <bnaveenbhat@gmail.com>
2026-07-27 12:39:53 +0530

    Release 2.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.qspapps.remindermate"
         minSdk = 29
         targetSdk = 37
-        versionCode = 21
-        versionName = "2.0.20"
+        versionCode = 22
+        versionName = "2.0.21 "
 
         testInstrumentationRunner = "com.qspapps.remindermate.HiltTestRunner"
     }
