c109e15de694d987fc64d902d991cee4a9a3f497
Naveen Belkale <bnaveenbhat@gmail.com>
2026-08-31 19:58:50 +0530

    Upgrading version to 2.0.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.qspapps.remindermate"
         minSdk = 29
         targetSdk = 37
-        versionCode = 23
-        versionName = "2.0.22"
+        versionCode = 24
+        versionName = "2.0.23"
 
         testInstrumentationRunner = "com.qspapps.remindermate.HiltTestRunner"
     }
